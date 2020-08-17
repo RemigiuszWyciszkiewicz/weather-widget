fd29423876881759b66ce462ebd2457c4211beb7
Remigiusz Wyciszkiewicz <remigiusz.wyciszkiewicz@asseco.pl>
2020-08-17 15:31:57 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   weather_api_key: '6f1a243886425bbb2d620ed0891ba61e',
-  weather_api_url: 'http://api.openweathermap.org/data/2.5/weather',
+  weather_api_url: 'https://api.openweathermap.org/data/2.5/weather',
 };
